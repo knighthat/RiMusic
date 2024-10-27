dde61e22c6f85da56ee405e7c747815a4ca6e9c6
knighthat <knightchanelgaming@gmail.com>
2024-10-27 01:28:24 -0500

    introduce kbuild flavor

--- compose-reordering/build.gradle.kts
+++ compose-reordering/build.gradle.kts
@@ -22,9 +22,7 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        create("kbuild") {
         }
     }
 
