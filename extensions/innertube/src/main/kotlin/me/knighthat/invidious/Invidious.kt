b1d42e3fe509b28e44f89224ec5e36d85bdc0be0
knighthat <knightchanelgaming@gmail.com>
2024-11-02 02:18:22 -0500

    extract get reachable instances

--- extensions/innertube/src/main/kotlin/me/knighthat/invidious/Invidious.kt
+++ extensions/innertube/src/main/kotlin/me/knighthat/invidious/Invidious.kt
@@ -28,6 +28,15 @@ object Invidious {
     private lateinit var API_INSTANCES: Array<String>
     private lateinit var UNREACHABLE_INSTANCES: MutableList<Regex>
 
+    internal val REACHABLE_INSTANCES: Collection<String>
+        get() = API_INSTANCES.filter {
+            for ( regex in UNREACHABLE_INSTANCES)
+                if ( regex.matches(it) )
+                    return@filter false
+
+            true
+        }
+
     suspend fun fetchInvidiousInstances( unofficial: Boolean ) {
         val url = if( unofficial ) UNOFFICIAL_PUBLIC_INSTANCES else VERIFIED_PUBLIC_INSTANCES
 
