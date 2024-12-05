70bd777108ba27401c2e7534c44e1821ceb37306
knighthat <contact@knighthat.me>
2024-12-05 12:09:50 -0600

    apply default value to constructor

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/ExoPlayerDiskCacheMaxSize.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/ExoPlayerDiskCacheMaxSize.kt
@@ -5,29 +5,29 @@ import androidx.compose.ui.res.stringResource
 import it.fast4x.rimusic.R
 import me.knighthat.enums.TextView
 
-enum class ExoPlayerDiskCacheMaxSize: TextView {
-    `Disabled`,
-    `32MB`,
-    `512MB`,
-    `1GB`,
-    `2GB`,
-    `4GB`,
-    `8GB`,
-    Unlimited,
-    Custom;
+enum class ExoPlayerDiskCacheMaxSize(
+    val megabytes: Int
+): TextView {
 
-    val bytes: Long
-        get() = when (this) {
-            Disabled -> 1
-            `32MB` -> 32
-            `512MB` -> 512
-            `1GB` -> 1024
-            `2GB` -> 2048
-            `4GB` -> 4096
-            `8GB` -> 8192
-            Unlimited -> 0
-            Custom -> 1000000
-        } * 1000 * 1000L
+    `Disabled`( 1 ),
+    
+    `32MB`( 32 ),
+    
+    `512MB`( 512 ),
+    
+    `1GB`( 1024 ),
+    
+    `2GB`( 2048 ),
+    
+    `4GB`( 4096 ),
+    
+    `8GB`( 8192 ),
+    
+    Unlimited( 0 ),
+    
+    Custom( 1_000_000 );        // YES! This is a valid format in Kotlin
+
+    val bytes: Long = this.megabytes * 1000L * 1000
 
     override val text: String
         @Composable
