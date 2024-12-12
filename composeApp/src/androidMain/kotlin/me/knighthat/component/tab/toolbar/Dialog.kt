b48a7707d7258855a483c9dcf8910de0129177ad
knighthat <knightchanelgaming@gmail.com>
2024-12-12 08:35:35 -0600

    define 'magic values' of dialog

--- composeApp/src/androidMain/kotlin/me/knighthat/component/tab/toolbar/Dialog.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/component/tab/toolbar/Dialog.kt
@@ -1,8 +1,20 @@
 package me.knighthat.component.tab.toolbar
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 interface Dialog {
+
+    companion object {
+
+        const val OUTER_PADDING = 10
+        const val INNER_PADDING = 10
+        const val HEIGHT = 190
+        const val TITLE_VERTICAL_PADDING = 8
+        const val TITLE_HORIZONTAL_PADDING = 24
+        val SHAPE = RoundedCornerShape( 8.dp )
+    }
 
     var isActive: Boolean
     @get:Composable
