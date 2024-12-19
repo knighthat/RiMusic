18fc3d321c3349cc057cd228fb7551ae9b2c9448
knighthat <contact@knighthat.me>
2024-12-18 23:08:00 -0600

    introduce repository's constant variables

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/settings/About.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/settings/About.kt
@@ -1,25 +1,10 @@
 package it.fast4x.rimusic.ui.screens.settings
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,22 +17,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import it.fast4x.rimusic.R
+import it.fast4x.rimusic.colorPalette
 import it.fast4x.rimusic.enums.NavigationBarPosition
-import it.fast4x.rimusic.extensions.contributors.ShowDevelopers
-import it.fast4x.rimusic.extensions.contributors.ShowTranslators
-import it.fast4x.rimusic.extensions.contributors.countDevelopers
-import it.fast4x.rimusic.extensions.contributors.countTranslators
 import it.fast4x.rimusic.enums.UiType
-import it.fast4x.rimusic.ui.components.themed.HeaderWithIcon
+import it.fast4x.rimusic.extensions.contributors.*
+import it.fast4x.rimusic.typography
 import it.fast4x.rimusic.ui.components.themed.Title
 import it.fast4x.rimusic.ui.styling.Dimensions
 import it.fast4x.rimusic.utils.bold
 import it.fast4x.rimusic.utils.getVersionName
 import it.fast4x.rimusic.utils.secondary
-import it.fast4x.rimusic.colorPalette
-import it.fast4x.rimusic.typography
-
-const val GITHUB_URL = "https://github.com/knighthat/RiMusic"
+import me.knighthat.util.Repository
 
 @ExperimentalAnimationApi
 @Composable
@@ -77,15 +57,16 @@ fun About() {
         ) {
             // Info icon with white circle around it
             Box(
-                modifier = Modifier.padding( end = 5.dp )                           // A lil space after icon
-                                   .size( 24.dp )
-                                   .border(
-                                       BorderStroke( 1.dp, colorPalette().text ),
-                                       shape = CircleShape
-                                   )
-                                   .padding( 1.dp )                                 // Space within border
-                                   .fillMaxSize()
-                                   .align( Alignment.CenterVertically )
+                modifier = Modifier
+                    .padding(end = 5.dp)                           // A lil space after icon
+                    .size(24.dp)
+                    .border(
+                        BorderStroke(1.dp, colorPalette().text),
+                        shape = CircleShape
+                    )
+                    .padding(1.dp)                                 // Space within border
+                    .fillMaxSize()
+                    .align(Alignment.CenterVertically)
             ) {
                 Icon(
                     painter = painterResource( R.drawable.information ),
@@ -117,7 +98,7 @@ fun About() {
             )
             Row(
                 Modifier.clickable {
-                    uriHandler.openUri( GITHUB_URL )
+                    uriHandler.openUri( Repository.OWNER_URL )
                 }
             ) {
                 Icon(
@@ -143,7 +124,7 @@ fun About() {
             title = stringResource(R.string.report_an_issue),
             text = stringResource(R.string.you_will_be_redirected_to_github),
             onClick = {
-                uriHandler.openUri("$GITHUB_URL/issues/new?assignees=&labels=bug&template=bug_report.yaml")
+                uriHandler.openUri("${Repository.REPO_URL}/issues/new?assignees=&labels=bug&template=bug_report.yaml")
             }
         )
 
@@ -151,7 +132,7 @@ fun About() {
             title = stringResource(R.string.request_a_feature_or_suggest_an_idea),
             text = stringResource(R.string.you_will_be_redirected_to_github),
             onClick = {
-                uriHandler.openUri("$GITHUB_URL/issues/new?assignees=&labels=feature_request&template=feature_request.yaml")
+                uriHandler.openUri("${Repository.REPO_URL}/issues/new?assignees=&labels=feature_request&template=feature_request.yaml")
             }
         )
 
