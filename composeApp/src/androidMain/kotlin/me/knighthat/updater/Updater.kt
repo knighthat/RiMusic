027913ecfa511c39ea64f7ae6a9973b7424f107f
knighthat <contact@knighthat.me>
2024-12-19 00:24:53 -0600

    update dialog

--- composeApp/src/androidMain/kotlin/me/knighthat/updater/Updater.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/updater/Updater.kt
@@ -1,10 +1,21 @@
 package me.knighthat.updater
 
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalUriHandler
-import it.fast4x.rimusic.BuildConfig
-import it.fast4x.rimusic.ui.components.tab.toolbar.ConfirmDialog
-import it.fast4x.rimusic.ui.components.themed.NewVersionDialog
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import it.fast4x.rimusic.*
+import it.fast4x.rimusic.R
+import it.fast4x.rimusic.enums.CheckUpdateState
+import it.fast4x.rimusic.ui.components.themed.SecondaryTextButton
+import it.fast4x.rimusic.ui.screens.settings.EnumValueSelectorSettingsEntry
+import it.fast4x.rimusic.ui.screens.settings.SettingsDescription
+import it.fast4x.rimusic.utils.checkUpdateStateKey
+import it.fast4x.rimusic.utils.rememberPreference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -58,7 +69,7 @@ object Updater {
     suspend fun fetchUpdate() = withContext( Dispatchers.IO ) {
         val client = OkHttpClient()
 
-        val tagUrl = "${Repository.GITHUB_API}/repos${Repository.TAG_PATH}"
+        val tagUrl = "${Repository.GITHUB_API}/repos${Repository.API_RELEASE_PATH}"
         val request = Request.Builder().url( tagUrl ).build()
         val response = client.newCall( request ).execute()
 
@@ -72,40 +83,46 @@ object Updater {
         }
     }
 
-    @Composable
-    fun CheckForUpdate() {
-        val uriHandler = LocalUriHandler.current
-
-        val updateDialog = object: ConfirmDialog {
-            override fun onConfirm() {
-                uriHandler.openUri( "$GITHUB$TAG_PATH")
-            }
-
-            override var isActive: Boolean by remember { mutableStateOf( false ) }
-            override val dialogTitle: String
-                @Composable
-                get() = ""
-
-            @Composable
-            override fun Render() {
-                if( !isActive ) return
-
-                NewVersionDialog(
-                    updatedProductName = "RiMusic-KBuild",
-                    updatedVersionCode = BuildConfig.VERSION_CODE,
-                    updatedVersionName = upstreamBuildDate.first.toString(),
-                    onDismiss = ::onDismiss
-                )
-            }
-        }
-        updateDialog.Render()
-
+    fun checkForUpdate() {
         if( !::upstreamBuildDate.isInitialized )
-            runBlocking { fetchUpdate() }
+            runBlocking( Dispatchers.IO ) { fetchUpdate() }
 
         val localBuildDate = BuildConfig.BUILD_DATE.toUInt()
         val canBeUpdated = localBuildDate < upstreamBuildDate.first
 
-        updateDialog.isActive = canBeUpdated
+        NewUpdateAvailableDialog.isActive = canBeUpdated
+    }
+
+    @Composable
+    fun SettingEntry() {
+        var checkUpdateState by rememberPreference( checkUpdateStateKey, CheckUpdateState.Disabled )
+
+        Row( Modifier.fillMaxWidth() ) {
+            EnumValueSelectorSettingsEntry(
+                title = stringResource( R.string.enable_check_for_update ),
+                selectedValue = checkUpdateState,
+                onValueSelected = { checkUpdateState = it },
+                valueText = { it.text },
+                modifier = Modifier.weight( 1f )
+            )
+
+            // Slide in from right + fade in effect. Slide out from left + fade out effect
+            AnimatedVisibility(
+                visible = checkUpdateState != CheckUpdateState.Disabled,
+                enter = slideInHorizontally(initialOffsetX = { it }) + fadeIn(initialAlpha = 0f),
+                exit = slideOutHorizontally(targetOffsetX = { it }) + fadeOut(targetAlpha = 0f)
+            ) {
+                SecondaryTextButton(
+                    text = stringResource( R.string.info_check_update_now ),
+                    onClick = {
+                        runBlocking( Dispatchers.IO ) { fetchUpdate() }       // Force fetch new update
+                        checkForUpdate()
+                    },
+                    modifier = Modifier.padding( end = 24.dp )
+                )
+            }
+        }
+
+        SettingsDescription( text = stringResource(R.string.when_enabled_a_new_version_is_checked_and_notified_during_startup) )
     }
 }
